3926c2c2df2cb8aace57547b18a25e306297aba8
wxwingim <78482070+wxwingim@users.noreply.github.com>
2022-05-12 11:11:39 +0300

    Update ServicesPage.tsx

--- frontend/src/components/Main/ServicesPage.tsx
+++ frontend/src/components/Main/ServicesPage.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import "./../custom.css";
+
+import ServiceTable from './Tables/ServiceTable';
 
 const ServicesPage = () => {
 
     return(
-        <div className='pt-10'>Services</div>
+        <Container >
+            <Row className="justify-content-center">
+                <Col md={9} className='p-5'>
+                    <ServiceTable />
+                </Col>             
+            </Row>           
+        </Container>       
     );
 }
 
